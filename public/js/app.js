fbb9a86a1cd5327077eb66eb48db03a8a06fe18f
Yejiel Wahnich <yejielw@hotmail.com>
2017-05-02 14:29:15 +0300

    added roles

--- public/js/app.js
+++ public/js/app.js
@@ -18,72 +18,72 @@ var app = angular.module('meanMapApp', [
 
 // Configures Angular routing -- showing the relevant view and controller when needed.
 //'authProvider','$httpProvider', 'jwtInterceptorProvider',
-app.config(['$stateProvider', '$urlRouterProvider', '$locationProvider','$provide',
-    function ($stateProvider, $urlRouterProvider, $locationProvider) {
+app.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', '$provide',
+    function($stateProvider, $urlRouterProvider, $locationProvider) {
 
-    $locationProvider.html5Mode(true);
+        $locationProvider.html5Mode(true);
 
-    $stateProvider
+        $stateProvider
         // Join Team Control Panel
-        .state('map.join', {
-            url: '/join',
-            controller: 'authCtrl',
-            templateUrl: 'partials/addForm.html',
+            .state('map.join', {
+                url: '/join',
+                controller: 'authCtrl',
+                templateUrl: 'partials/addForm.html',
 
-            // Find Home Cooks Control Panel
-        })
-        .state('list', {
-            url: '/list',
-            controller: 'foodController',
-            templateUrl: 'partials/list.html',
+                // Find Home Cooks Control Panel
+            })
+            .state('list', {
+                url: '/list',
+                controller: 'foodController',
+                templateUrl: 'partials/list.html',
 
-            // All else forward to the Join Home Cook Team Control Panel
-        })
-        .state('orderForm', {
-            url: '/orderForm',
-            controller: 'foodController',
-            templateUrl: 'partials/orderForm.html',
+                // All else forward to the Join Home Cook Team Control Panel
+            })
+            .state('orderForm', {
+                url: '/orderForm',
+                controller: 'foodController',
+                templateUrl: 'partials/orderForm.html',
 
-            // All else forward to the Join Home Cook Team Control Panel
-        })
-        .state('home', {
-            url: '/home',
-            templateUrl: 'js/components/home/home.tpl.html',
-        })
-        .state('profile', {
-            url: '/profile',
-            templateUrl: 'js/components/profile/profile.tpl.html',
-            controller: 'profileController as user'
-        })
-        .state('map', {
-            url: '/map',
-            templateUrl: 'partials/map.html',
-            controller: 'queryCtrl'
-        })
-        .state('map.find', {
-            url: '/find',
-            templateUrl: 'partials/queryForm.html'
-            // All else forward to the Join Home Cook Team Control Panel
-        })
-        .state('register', {
-            url: '/register',
-            templateUrl: '/partials/userRegistration.html',
-            controller: 'authCtrl'
-        })
-        .state('login', {
-        url: '/login',
-        templateUrl: '/partials/login.html',
-        controller: 'authCtrl'
-        })
-    // lockProvider.init({
-    //     clientID: '55GqM7lzjXdbdFhkBlb7BSUQY2IvRUVk',
-    //     domain: 'kinamartinez.eu.auth0.com',
-    //     options: {
-    //         _idTokenVerification: false
-    //     }
-    // });
+                // All else forward to the Join Home Cook Team Control Panel
+            })
+            .state('home', {
+                url: '/home',
+                templateUrl: 'js/components/home/home.tpl.html',
+            })
+            .state('profile', {
+                url: '/profile',
+                templateUrl: 'js/components/profile/profile.tpl.html',
+                controller: 'profileController as user'
+            })
+            .state('map', {
+                url: '/map',
+                templateUrl: 'partials/map.html',
+                controller: 'queryCtrl'
+            })
+            .state('map.find', {
+                url: '/find',
+                templateUrl: 'partials/queryForm.html',
+                controller: 'authCtrl'
+                    // All else forward to the Join Home Cook Team Control Panel
+            })
+            .state('register', {
+                url: '/register',
+                templateUrl: '/partials/userRegistration.html',
+                controller: 'authCtrl'
+            })
+            .state('login', {
+                url: '/login',
+                templateUrl: '/partials/login.html',
+                controller: 'authCtrl'
+            })
+            // lockProvider.init({
+            //     clientID: '55GqM7lzjXdbdFhkBlb7BSUQY2IvRUVk',
+            //     domain: 'kinamartinez.eu.auth0.com',
+            //     options: {
+            //         _idTokenVerification: false
+            //     }
+            // });
 
-    $urlRouterProvider.otherwise('/home');
-}]);
-
-
+        $urlRouterProvider.otherwise('/home');
+    }
+]);
--- public/js/authCtrl.js
+++ public/js/authCtrl.js
@@ -1,19 +1,19 @@
 app.controller('authCtrl', function($scope, authFactory, $state, $rootScope, geolocation, gservice) {
 
-   $scope.user = {};
+    $scope.user = {};
     let coords = {};
     let lat = 0;
     let long = 0;
 
-   // Set initial coordinates to the center of the Israel
+    // Set initial coordinates to the center of the Israel
     $scope.user.latitude = 32.04;
     $scope.user.longitude = 34.46;
 
     // Get User's actual coordinates based on HTML5 at window load
-    geolocation.getLocation().then(function(data){
+    geolocation.getLocation().then(function(data) {
 
         // Set the latitude and longitude equal to the HTML5 coordinates
-        coords = {lat:data.coords.latitude, long:data.coords.longitude};
+        coords = { lat: data.coords.latitude, long: data.coords.longitude };
 
         // Display coordinates in location textboxes rounded to three decimal points
         $scope.user.latitude = parseFloat(coords.long).toFixed(3);
@@ -28,31 +28,40 @@ app.controller('authCtrl', function($scope, authFactory, $state, $rootScope, geo
 
     // Functions
     // Get coordinates based on mouse click. When a click event is detected....
-    $rootScope.$on("clicked", function(){
+    $rootScope.$on("clicked", function() {
 
         // Run the gservice functions associated with identifying coordinates
-        $scope.$apply(function(){
+        $scope.$apply(function() {
             $scope.user.latitude = parseFloat(gservice.clickLat).toFixed(3);
             $scope.user.longitude = parseFloat(gservice.clickLong).toFixed(3);
             $scope.formData.htmlverified = "Hey (You are not in this location...)";
         });
     });
 
-  $scope.register = function() {
-    authFactory.register($scope.user)
-      .then(function() {
-          console.log($scope.user)
-        $state.go('home');
-      }, function(err) {
-        alert(err.data.message);
-      });
-  };
-  $scope.login = function() {
-    authFactory.login($scope.user)
-      .then(function() {
-        $state.go('home');
-      }, function(err) {
-        alert(err.data);
-      });
-  }
+    $scope.register = function() {
+        authFactory.register($scope.user)
+            .then(function() {
+                console.log($scope.user)
+                $state.go('home');
+            }, function(err) {
+                alert(err.data.message);
+            });
+    };
+    $scope.registerCook = function() {
+        authFactory.register($scope.user)
+            .then(function() {
+                console.log($scope.user)
+                $state.go('home');
+            }, function(err) {
+                alert(err.data.message);
+            });
+    };
+    $scope.login = function() {
+        authFactory.login($scope.user)
+            .then(function() {
+                $state.go('home');
+            }, function(err) {
+                alert(err.data);
+            });
+    }
 });
