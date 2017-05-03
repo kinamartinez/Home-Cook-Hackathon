74023c7a54fff842b07161b306b63d90a502618d
Yejiel Wahnich <yejielw@hotmail.com>
2017-05-03 16:53:28 +0300

    basic UI working

--- public/js/masterController.js
+++ public/js/masterController.js
@@ -2,13 +2,4 @@ app.controller('masterController', function($scope, $rootScope, authFactory) {
     authFactory.getCurrentUser().then(function(data) {
         $rootScope.currentUser = data
     });
-    $scope.logout = function() {
-        authFactory.logout($rootScope.currentUser)
-            .then(function(user) {
-                $rootScope.currentUser = null;
-                $state.go('home');
-            }, function(err) {
-                alert(err.data);
-            });
-    }
 });
